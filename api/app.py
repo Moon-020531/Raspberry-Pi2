060d6afb1b1fe1bda787c0338ace7e4d137baf94
Moon-020531 <msk7676@naver.com>
2026-02-19 12:11:17 +0900

    추가

--- api/app.py
+++ api/app.py
@@ -1,74 +1,6 @@
-from flask import Flask
 from flask import Flask, request,jsonify
 from flask.json.provider import DefaultJSONProvider
 import json
-
-
-
-app = (Flask)(__name__)
-
-@app.route("/ping", methods=['GET'])
-
-def ping():
-    return "pong"
-
-
-
-app = (Flask)(__name__)
-app.users = {}
-app.id_count = 1 
-
-@app.route("/ping", methods=['GET'])
-def ping():
-    return "pong"
-
-@app.route("/sign-up", methods=['POST'])
-def sign_up():
-    new_user = request.json
-    new_user["id"] = app.id_count
-    app.users[app.id_count] = new_user
-    app.id_count = app.id_count + 1
-
-    return jsonify(new_user)
-
-app = (Flask)(__name__)
-app.users = {}
-app.id_count = 1 
-app.tweets = []
-
-@app.route("/ping", methods=['GET'])
-def ping():
-    return "pong"
-
-@app.route("/sign-up", methods=['POST'])
-def sign_up():
-    new_user = request.json                 # 회원정보 읽기
-    new_user["id"] = app.id_count
-    app.users[app.id_count] = new_user
-    app.id_count = app.id_count + 1
-
-    return jsonify(new_user)                # json형태로 전송 
-
-@app.route('/tweet', methods=['POST'])
-def tweet():
-    payload = request.json
-    user_id = int(payload['id'])
-    tweet = payload['tweet']
-
-    if user_id not in app.users:
-        return '사용자가 존재하지 않습니다.', 400
-
-    if len(tweet) > 300:
-        return '300자를 초과했습니다.' , 400
-    
-    user_id = int(payload['id'])
-
-    app.tweets.append({
-        'user_id' : user_id,
-        'tweet'   : tweet
-    })
-
-    return '', 200
 
 class CustomJSONProvider(DefaultJSONProvider):
     def default(self, obj):
@@ -145,7 +77,6 @@ def unfollow():
     user.setdefault('follow', set()).discard(user_id_to_follow) #키가 존재하지 않으면 디폴트값을 저장하고, 만일 키가 이미 존재하면 해당 값을 읽어들이는 기능 
 
     return jsonify(user)
-
 
 @app.route('/timeline/<int:user_id>', methods=['GET'])
 def timeline(user_id):
